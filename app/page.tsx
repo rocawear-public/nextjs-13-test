934bfe8a51ff267b06ab4e822ef7433caf02923f
rocawear <anttivepp@gmail.com>
2022-10-26 01:48:21 +0300

    error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex flex-col">
       <h1>Home</h1>
+      <div className="flex flex-row gap-4">
+        <Link className="py-2 px-4 bg-green-400" href="/products">
+          Products
+        </Link>
+        <Link className="py-2 px-4 bg-green-400" href="/nested-products">
+          Nested Products
+        </Link>
+      </div>
     </main>
   );
 }
